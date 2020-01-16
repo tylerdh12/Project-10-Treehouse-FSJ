b3c965552b893956b309d20111ad9dc7483e562a
Tyler Harper <tylerdh12@gmail.com>
2020-01-16 13:13:15 -0800

    setup form for users and courses

---
+++ client/src/Data.js
@@ -0,0 +1,45 @@
+import config from "./config";
+
+export default class Data {
+  api(
+    path,
+    method = "GET",
+    body = null,
+    requiresAuth = false,
+    credentials = null
+  ) {
+    const url = config.apiBaseUrl + path;
+
+    const options = {
+      method,
+      headers: {
+        "Content-Type": "application/json; charset=utf-8"
+      }
+    };
+
+    if (body !== null) {
+      options.body = JSON.stringify(body);
+    }
+
+    if (requiresAuth) {
+      const encodedCredentials = btoa(
+        `${credentials.username}:${credentials.password}`
+      );
+
+      options.headers["Authorization"] = `Basic ${encodedCredentials}`;
+    }
+
+    return fetch(url, options);
+  }
+
+  async getCourses() {
+    const response = await this.api(`/courses`, "GET", null, true);
+    if (response.status === 200) {
+      return response.json().then(data => data);
+    } else if (response.status === 401) {
+      return null;
+    } else {
+      throw new Error();
+    }
+  }
+}
